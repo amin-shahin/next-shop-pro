6de0012344bd821fb91e3c024a4306d29af244e8
Amin Shahin <aminshahin.web@gmail.com>
2025-02-17 11:52:54 +0330

    changes styles auth layout

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,12 +1,16 @@
 import vazirFont from "@/constants/localFont";
 import "@/styles/globals.css";
+import Link from "next/link";
 
 function layoutAuth({ children }) {
   return (
     <html lang="fa" dir="rtl" suppressHydrationWarning>
       <body className={`${vazirFont.variable}  font-sans `}>
-        <button>بازگشت به صفحه اصلی</button>
-        <div>{children}</div>
+        <Link href={'/'}>بازگشت به صفحه اصلی</Link>
+        <div className="flex items-center justify-center w-full min-h-svh">
+             <div>{children}</div>
+        </div>
+       
       </body>
     </html>
   );
