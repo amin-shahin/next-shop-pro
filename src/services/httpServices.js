27ddb0b7d4336ef4f9955f125e60bab07708a1fb
Amin Shahin <aminshahin.web@gmail.com>
2025-02-20 20:15:59 +0330

    handle refreshToken in authServices

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -5,6 +5,33 @@ const appApi = axios.create({
   withCredentials: true,
 });
 
+appApi.interceptors.request.use(
+  (res) => res,
+  (err) => Promise.reject(err)
+);
+
+appApi.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalConfig = err.config;
+    try {
+      if (err.response.status === "401" && !originalConfig._retry) {
+        originalConfig._retry = true;
+      }
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/user/refresh-token`,
+        { withCredentials: true }
+      );
+      if (data) {
+        return appApi(originalConfig);
+      }
+    } catch (error) {
+      return Promise.reject(error);
+    }
+    return Promise.reject(err)
+  }
+);
+
 const http = {
   get: appApi.get,
   post: appApi.post,
